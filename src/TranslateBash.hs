c98edb22c033842f51288c4179ee94ef524fe555
Paul Biggar <paul.biggar@gmail.com>
2016-01-17 16:23:50 -0600

    Handle heredocs more generally, handle AssignBuiltin, expand SimpleCommand to more than 1 Assign, reorder slightly and add nicer commands

--- src/TranslateBash.hs
+++ src/TranslateBash.hs
@@ -15,6 +15,8 @@ import qualified Text.Groom as G
 --import qualified Data.List.Utils as U
 import qualified Data.Maybe as Maybe
 
+
+-- | Debugging
 debugStr :: (Show a, BashPretty.Pretty a) => a -> String -> String
 debugStr x reason = "TODO (" ++ reason ++ ") - " ++ (BashPretty.prettyText x) ++ " - " ++ (show x)
 
@@ -24,6 +26,7 @@ debug x reason = Debug (debugStr x reason)
 debugWithType :: (Show a, Typeable.Typeable a, BashPretty.Pretty a) => a -> String -> Expr
 debugWithType x reason = debug x (reason ++ " " ++ (show (Typeable.typeOf x)))
 
+-- | The AST definition
 data Program = Program Expr deriving (Show)
 data Expr = Command
             | If Expr Expr Expr
@@ -51,6 +54,7 @@ data Expr = Command
 data LValue = LVar String
               deriving (Show)
 
+-- | Lists
 convertList :: S.List -> Expr
 -- TODO: ignoring pipeline args
 convertList (S.List stmts) =
@@ -60,7 +64,7 @@ listOrExpr :: [Expr] -> Expr
 listOrExpr (e:[]) = e
 listOrExpr es = List es
 
-
+-- | Pipelines
 convertAndOr :: S.AndOr -> Expr
 convertAndOr (S.Last p) = convertPipeline p
 convertAndOr (S.And p ao) = And (convertPipeline p) (convertAndOr ao)
@@ -69,114 +73,58 @@ convertAndOr (S.Or p ao) = Or (convertPipeline p) (convertAndOr ao)
 convertPipeline :: S.Pipeline -> Expr
 -- ignored timing and inverted. I think this is right.
 convertPipeline (S.Pipeline _ _ _ cs) =
-    listOrExpr [ convertCommand sc rs | (S.Command sc rs) <- cs ]
+    listOrExpr [ convertShellCommand sc rs | (S.Command sc rs) <- cs ]
+
+-- | Commands
+convertShellCommand :: S.ShellCommand -> [S.Redir] -> Expr
+convertShellCommand (S.If cond l1 Nothing) [] = If
+                                                (convertList cond)
+                                                (convertList l1)
+                                                (listOrExpr [Nop]) -- TODO: is Maybe nicer here?
+
+convertShellCommand (S.If cond l1 (Just l2)) [] = If
+                                                  (convertList cond)
+                                                  (convertList l1)
+                                                  (convertList l2)
+
+-- TODO: this is the only place that handles heredocs. Everywhere should.
+convertShellCommand (S.SimpleCommand as ws) rs = convertSimpleCommand as (combineHeredoc ws rs)
+
+convertShellCommand (S.AssignBuiltin w es) []
+    | w == (W.fromString "local") = listOrExpr (map convertAssignOrWord es)
+    | otherwise = debugWithType w "ccscab"
+
+convertShellCommand (S.Cond e) [] = convertCondExpr e
+convertShellCommand (S.FunctionDef name cmds) [] =
+    FunctionDef name (convertList cmds)
+
+convertShellCommand (S.For v wl cmds) [] =
+    For (LVar v) (convertWordList wl) (convertList cmds)
+
+convertShellCommand x rs = debugWithType x ("cc" ++ (show rs))
 
 
-convertCommand :: S.ShellCommand -> [S.Redir] -> Expr
-convertCommand (S.If cond l1 Nothing) [] = If
-                                           (convertList cond)
-                                           (convertList l1)
-                                           (listOrExpr [Nop]) -- TODO: is Maybe nicer here?
-
-convertCommand (S.If cond l1 (Just l2)) [] = If
-                                             (convertList cond)
-                                             (convertList l1)
-                                             (convertList l2)
-
-convertCommand (S.SimpleCommand [] ws) rs = convertWords (combineHeredoc ws rs)
-
+-- | SimpleCommands (assignments)
+convertSimpleCommand :: [S.Assign] -> [W.Word] -> Expr
+convertSimpleCommand as [] = listOrExpr (map convertAssign as)
+convertSimpleCommand as ws = listOrExpr ((map convertAssign as) ++ [(convertWords ws)])
 
 -- TODO: parameter doesn't take subscript
 -- TODO: assignment doesn't handle +=
--- TODO: what are the rest of the words doing here?
--- TODO: doesn't handle multiple assignment
-convertCommand (S.SimpleCommand
-                [(S.Assign
-                  (W.Parameter name _)
-                  S.Equals
-                  (S.RValue r))]
-                _)
-                [] =
+convertAssign :: S.Assign -> Expr
+convertAssign (S.Assign (W.Parameter name _) S.Equals (S.RValue r)) =
   Assignment (LVar name) (convertWord r)
 
-convertCommand (S.Cond e) [] = convertCondExpr e
-convertCommand (S.FunctionDef name cmds) [] =
-    FunctionDef name (convertList cmds)
-
-convertCommand (S.For v wl cmds) [] =
-    For (LVar v) (convertWordList wl) (convertList cmds)
-
-convertCommand x rs = debugWithType x ("cc" ++ (show rs))
+convertAssignOrWord :: Either S.Assign W.Word -> Expr
+convertAssignOrWord (Left a) = convertAssign a
+convertAssignOrWord (Right w) = convertWord w
 
 
+
+-- | WordLists and Words and Spans
 convertWordList :: S.WordList -> Expr
 convertWordList S.Args = (Str "$@") -- TODO
 convertWordList (S.WordList wl) = listOrExpr (map convertWord wl)
-
--- when given a heredoc, convert it into a list of words
-combineHeredoc :: [W.Word] -> [S.Redir] -> [W.Word]
-combineHeredoc ws rs = ws ++ ns
-    where ns = Maybe.catMaybes $ map hd2word rs
-
-
-hd2word :: S.Redir -> Maybe W.Word
-hd2word (S.Heredoc {S.hereDocument=hd}) = Just hd
-hd2word _ = Nothing
-
-
-convertCondExpr :: C.CondExpr W.Word -> Expr
-convertCondExpr (C.Not e) = Not (convertCondExpr e)
-convertCondExpr (C.Unary uop w) =
-    FunctionInvocation (uop2FunctionName uop) [convertWord w]
-convertCondExpr (C.Binary l C.StrEQ r) = Equals (convertWord l) (convertWord r)
-convertCondExpr (C.Binary l C.ArithEQ r) = Equals (convertWord l) (convertWord r)
-convertCondExpr (C.Binary l C.StrNE r) = Not (Equals (convertWord l) (convertWord r))
-convertCondExpr (C.Binary l C.ArithNE r) = Not (Equals (convertWord l) (convertWord r))
-convertCondExpr (C.Binary l C.StrLT r) = LessThan (convertWord l) (convertWord r)
-convertCondExpr (C.Binary l C.ArithLT r) = LessThan (convertWord l) (convertWord r)
-convertCondExpr (C.Binary l C.StrGT r) = GreaterThan (convertWord l) (convertWord r)
-convertCondExpr (C.Binary l C.ArithLE r) = Not (GreaterThan (convertWord l) (convertWord r))
-convertCondExpr (C.Binary l C.ArithGE r) = Not (LessThan (convertWord l) (convertWord r))
-convertCondExpr (C.Binary l bop r) = FunctionInvocation (bop2FunctionName bop) [convertWord l, convertWord r]
-
-
-convertCondExpr e = debugWithType e "ceEmpty"
-
-bop2FunctionName :: C.BinaryOp -> String
-bop2FunctionName C.SameFile = "file.same?"
-bop2FunctionName C.NewerThan = "file.newer_than?"
-bop2FunctionName C.OlderThan = "file.older_than?"
-bop2FunctionName C.StrMatch = "re.matches"
-bop2FunctionName _ = "FAIL"
-
-uop2FunctionName :: C.UnaryOp -> String
-uop2FunctionName C.BlockFile = "file.isBlockFile"
-uop2FunctionName C.CharacterFile = "file.isCharacterFile"
-uop2FunctionName C.Directory = "file.is_directory?"
-uop2FunctionName C.FileExists = "file.exists?"
-uop2FunctionName C.RegularFile = "file.is_regular_file?"
-uop2FunctionName C.SetGID = "file.isSetGID"
-uop2FunctionName C.Sticky = "file.isSticky"
-uop2FunctionName C.NamedPipe = "file.isNamedPipe"
-uop2FunctionName C.Readable = "file.isReadable"
-uop2FunctionName C.FileSize = "file.isFileSize"
-uop2FunctionName C.Terminal = "file.isTerminal"
-uop2FunctionName C.SetUID = "file.isSetUID"
-uop2FunctionName C.Writable = "file.isWritable"
-uop2FunctionName C.Executable = "file.isExecutable"
-uop2FunctionName C.GroupOwned = "file.isGroupOwned"
-uop2FunctionName C.SymbolicLink = "file.isSymbolicLink"
-uop2FunctionName C.Modified = "file.isModified"
-uop2FunctionName C.UserOwned = "file.isUserOwned"
-uop2FunctionName C.Socket = "file.isSocket"
-uop2FunctionName C.ZeroString = "string.blank?"
-uop2FunctionName C.NonzeroString = "string.nonblank?"
-uop2FunctionName a = debugStr (show a) "uop2FunctionName"
--- TODO: these ones are a bit odd
--- uop2FunctionName C.Optname =
--- uop2FunctionName C.Varname =
-
-
 
 
 convertWords :: [W.Word] -> Expr
@@ -185,19 +133,6 @@ convertWords ([W.Char '['] : ws)
     | otherwise = debugWithType ws "cw"
 convertWords (w:ws) = convertFunctionCall (convertString w) (map convertWord ws)
 convertWords ws@[] = debugWithType ws "cwEmpty"
-
-convertFunctionCall :: String -> [Expr] -> Expr
-convertFunctionCall "set" [(Str "-e")] = Nop
-convertFunctionCall "set" [(Str "+e")] = Nop
-convertFunctionCall name args = FunctionInvocation name args
-
-convertTest :: [W.Word] -> Expr
-convertTest ws = case condExpr of
-    Left  err -> Debug $ "doesn't parse" ++ (show err) ++ (show hacked)
-    Right e -> (convertCommand (S.Cond (convertStrCondExpr2WordCondExpr e)) [])
-    where condExpr = C.parseTestExpr hacked
-          hacked = hackTestExpr strs
-          strs = (map W.unquote ws)
 
 convertWord :: W.Word -> Expr
 convertWord ss = cConcat [convertSpan s | s <- ss]
@@ -233,6 +168,88 @@ convertString w = case (convertWord w) of
                     _ -> "TODO - couldnt get a string out of " ++ (show w)
 
 
+convertFunctionCall :: String -> [Expr] -> Expr
+convertFunctionCall "set" [(Str "-e")] = Nop
+convertFunctionCall "set" [(Str "+e")] = Nop
+convertFunctionCall name args = FunctionInvocation name args
+
+
+-- | Heredocs
+combineHeredoc :: [W.Word] -> [S.Redir] -> [W.Word]
+combineHeredoc ws rs = ws ++ ns
+    where ns = Maybe.catMaybes $ map hd2word rs
+
+hd2word :: S.Redir -> Maybe W.Word
+hd2word (S.Heredoc {S.hereDocument=hd}) = Just hd
+hd2word _ = Nothing
+
+-- | CondExprs
+convertCondExpr :: C.CondExpr W.Word -> Expr
+convertCondExpr (C.Not e) = Not (convertCondExpr e)
+convertCondExpr (C.Unary uop w) =
+    FunctionInvocation (uop2FunctionName uop) [convertWord w]
+convertCondExpr (C.Binary l C.StrEQ r) = Equals (convertWord l) (convertWord r)
+convertCondExpr (C.Binary l C.ArithEQ r) = Equals (convertWord l) (convertWord r)
+convertCondExpr (C.Binary l C.StrNE r) = Not (Equals (convertWord l) (convertWord r))
+convertCondExpr (C.Binary l C.ArithNE r) = Not (Equals (convertWord l) (convertWord r))
+convertCondExpr (C.Binary l C.StrLT r) = LessThan (convertWord l) (convertWord r)
+convertCondExpr (C.Binary l C.ArithLT r) = LessThan (convertWord l) (convertWord r)
+convertCondExpr (C.Binary l C.StrGT r) = GreaterThan (convertWord l) (convertWord r)
+convertCondExpr (C.Binary l C.ArithLE r) = Not (GreaterThan (convertWord l) (convertWord r))
+convertCondExpr (C.Binary l C.ArithGE r) = Not (LessThan (convertWord l) (convertWord r))
+convertCondExpr (C.Binary l bop r) = FunctionInvocation (bop2FunctionName bop) [convertWord l, convertWord r]
+
+convertCondExpr e = debugWithType e "ceEmpty"
+
+-- | Function names for BinaryOps
+bop2FunctionName :: C.BinaryOp -> String
+bop2FunctionName C.SameFile = "file.same?"
+bop2FunctionName C.NewerThan = "file.newer_than?"
+bop2FunctionName C.OlderThan = "file.older_than?"
+bop2FunctionName C.StrMatch = "re.matches"
+bop2FunctionName _ = "FAIL"
+
+-- | Function names for UnaryOps
+uop2FunctionName :: C.UnaryOp -> String
+uop2FunctionName C.BlockFile = "file.isBlockFile"
+uop2FunctionName C.CharacterFile = "file.isCharacterFile"
+uop2FunctionName C.Directory = "file.is_directory?"
+uop2FunctionName C.FileExists = "file.exists?"
+uop2FunctionName C.RegularFile = "file.is_regular_file?"
+uop2FunctionName C.SetGID = "file.isSetGID"
+uop2FunctionName C.Sticky = "file.isSticky"
+uop2FunctionName C.NamedPipe = "file.isNamedPipe"
+uop2FunctionName C.Readable = "file.isReadable"
+uop2FunctionName C.FileSize = "file.isFileSize"
+uop2FunctionName C.Terminal = "file.isTerminal"
+uop2FunctionName C.SetUID = "file.isSetUID"
+uop2FunctionName C.Writable = "file.isWritable"
+uop2FunctionName C.Executable = "file.isExecutable"
+uop2FunctionName C.GroupOwned = "file.isGroupOwned"
+uop2FunctionName C.SymbolicLink = "file.isSymbolicLink"
+uop2FunctionName C.Modified = "file.isModified"
+uop2FunctionName C.UserOwned = "file.isUserOwned"
+uop2FunctionName C.Socket = "file.isSocket"
+uop2FunctionName C.ZeroString = "string.blank?"
+uop2FunctionName C.NonzeroString = "string.nonblank?"
+uop2FunctionName a = debugStr (show a) "uop2FunctionName"
+-- TODO: these ones are a bit odd
+-- uop2FunctionName C.Optname =
+-- uop2FunctionName C.Varname =
+
+-- | Tests (handles test, '[' and '[[')
+convertTest :: [W.Word] -> Expr
+-- note that bash parses tests after it has expanded the arguments, and so you
+-- can't really statically parse this. For example, what does `[ "$x" a ]` do?
+-- Still, we can get this right when they are statically specified, and get the
+-- gist right in some cases otherwise.
+convertTest ws = case condExpr of
+    Left  err -> Debug $ "doesn't parse" ++ (show err) ++ (show hacked)
+    Right e -> (convertShellCommand (S.Cond (convertStrCondExpr2WordCondExpr e)) [])
+    where condExpr = C.parseTestExpr hacked
+          hacked = hackTestExpr strs
+          strs = (map W.unquote ws)
+
 -- break the bash rules to fix up mistakes
 hackTestExpr :: [String] -> [String]
 -- [ -a asd ] works, but [ ! -a asd] doesnt because -a is the "and" operator. -e does the same though.
@@ -251,6 +268,7 @@ csce2wce (C.And a b) = C.And (csce2wce a) (csce2wce b)
 csce2wce (C.Or a b) = C.Or (csce2wce a) (csce2wce b)
 
 
+-- | Turn lists of Strings or string components into a Str
 foldStrs :: [Expr] -> [Expr]
 foldStrs ((Str a) : (Str b) : ss) = foldStrs ((Str (a ++ b)) : ss)
 foldStrs (s : ss) = (s : foldStrs ss)
@@ -266,47 +284,52 @@ cConcat0 es = Concat es
 
 -- TODO:
 
--- buggy parsing
--- - parse DoubleQuoted strings
--- - heredocs dont remove leading tabs for <<-
+-- | possibly buggy parsing
+-- parse DoubleQuoted strings
+-- heredocs dont remove leading tabs for <<-
+-- when parsing AssignBuiltin, lists of words wont be handled as lists of words
 
--- handle builtins
--- - echo (-e)
--- - basename
--- - printf
--- - exit (convert to a number)
--- - $#, $1, etc
--- - nullglob and dotglob
--- - getopts
--- - set -e - how to allow failure to be handled well? Exceptions? We currently
---   just rip them out
+-- | handle builtins
+-- echo (-e)
+-- basename
+-- printf
+-- exit (convert to a number)
+-- $#, $1, etc
+-- nullglob and dotglob
+-- getopts
+-- set -e - how to allow failure to be handled well? Exceptions? We currently
+--   just rip them out for now
+-- type
 
 
--- replace shell utilities that are tricku to use
--- - awk
--- - sed
--- - cut
--- - expr
--- - grep?
--- - readlink?
--- - tr
--- - xargs?
--- - seq
+-- | replace shell utilities that are tricky to use
+-- awk
+-- sed
+-- cut
+-- expr
+-- grep?
+-- readlink?
+-- tr
+-- xargs?
+-- seq
+-- curl/wget into builtin
 
--- obvious improvements
--- - convert lists of echos into a single heredoc
--- - convert globals into params being passed around
--- - convert assignments of "0" and "1" into real bools
--- - find BASH_REMATCH and convert it into proper usage
--- - find duplicated code
--- - variable variables into hashtables
--- - turn bash RE into PCRE
--- - turn nested if/else into switches
+-- | obvious improvements
+-- convert lists of echos into a single heredoc
+-- convert globals into params being passed around
+-- convert assignments of "0" and "1" into real bools
+-- find BASH_REMATCH and convert it into proper usage
+-- find duplicated code
+-- variable variables into hashtables
+-- turn bash RE into PCRE
+-- turn nested if/else into switches
+-- exit code into integer
+-- if IFS is set, all bets are off
 
 translate :: String -> IO ()
 translate file = do
   src <- readFile file
-  case BashParse.parse "source" src of
+  case BashParse.parse file src of
     { Left err -> putStrLn (show err)
     ; Right ans -> do
         putStrLn (G.groom (Program (convertList ans)))
