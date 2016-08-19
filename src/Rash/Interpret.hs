05ad27314206dc02d79f060fb894daea6153bd77
Paul Biggar <paul.biggar@gmail.com>
2016-08-19 15:47:00 -0400

    Now you're cooking with Monad Transformers

--- src/Rash/Interpret.hs
+++ src/Rash/Interpret.hs
@@ -5,8 +5,15 @@ import System.Exit (ExitCode)
 import Rash.Bash2AST (translateFile)
 import Rash.AST
 import qualified Data.Either as Either
+import qualified Data.Map.Strict as Map
 import qualified System.Exit as Exit
+import qualified Control.Monad.Trans.State as State
+import Control.Monad.IO.Class (liftIO)
 
+data Value = Test
+
+type IState = Map.Map String Value
+type WithState = State.StateT IState IO Value
 
 interpretFile :: FilePath -> IO ExitCode
 interpretFile file = do
@@ -21,13 +28,24 @@ interpretFile file = do
         return $ Exit.ExitFailure (-1)
   return $ exitCode
 
-interpret :: Program -> IO ExitCode
-interpret = exeProgram
+convertToExitCode :: Value -> ExitCode
+convertToExitCode _ = Exit.ExitSuccess
 
-exeProgram :: Program -> IO ExitCode
+interpret :: Program -> IO ExitCode
+interpret program = do
+  (val, final) <- State.runStateT (exeProgram program) Map.empty
+  return $ convertToExitCode val
+
+exeProgram :: Program -> WithState
 exeProgram (Program e) = exeExpr e
 
-exeExpr :: Expr -> IO ExitCode
+exeExpr :: Expr -> WithState
+exeExpr (List es) = do
+  result <- mapM exeExpr es
+  return $ last result
+
+
+
 exeExpr e = do
-  print e
-  return $ Exit.ExitFailure (-1)
+  liftIO $ print "Something"
+  return $ Test
