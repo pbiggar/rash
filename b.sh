a3315cdbb5980ec6f0543be3dcb008877736859b
Paul Biggar <paul.biggar@gmail.com>
2016-09-12 14:50:22 -0400

    get processes nearly working

---
+++ b.sh
@@ -0,0 +1,1 @@
+ls | `cat` | wc -l | wc -c
--- src/rash/Interpreter.hs
+++ src/rash/Interpreter.hs
@@ -1,19 +1,26 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Rash.Interpreter where
 
 import qualified Data.Map.Strict as Map
 import qualified Control.Monad.Trans.State as State
 import           Control.Monad.IO.Class (liftIO)
 import qualified System.Exit as Exit
-import qualified System.Cmd as Cmd
+import qualified System.Process as Proc
+import qualified GHC.IO.Handle as Handle
+import qualified System.IO as IO
 
 import Rash.AST
+
+todo :: Show a => String -> a -> b
+--todo reason a = VTodo reason (show a)
+todo reason a = error (reason ++ ": " ++ (show a))
 
 data Value = VInt Int
            | VString String
            | VBool Bool
            | VExitCode Int
            | VNull
-           | VTest
+           | VTodo String String
            | VHash (Map.Map String Value)
            | VArray [Value]
            | VPacket Exit.ExitCode -- TODO stdout and stderr as streams
@@ -65,11 +72,20 @@ isTruthy (VBool b) = b
 isTruthy (VExitCode 0) = True
 isTruthy (VExitCode _) = False
 isTruthy VNull = False
-isTruthy VTest = False
+isTruthy (VTodo _ _) = False
 isTruthy (VArray _) = True
 isTruthy (VHash _) = True
-isTruthy (VPacket _) = error "should vpacket be truthy?"
+isTruthy vp@(VPacket _) = todo "should vpacket be truthy?" vp
 
+eval2Str :: Expr -> WithState
+eval2Str e = do
+    expr <- evalExpr e
+    return $ toString expr
+
+
+toString :: Value -> Value
+toString s@(VString _) = s
+toString v = todo "Not a string" v
 
 evalProgram :: Program -> WithState
 evalProgram (Program e) = evalExpr e
@@ -94,14 +110,14 @@ evalExpr (Equals l r) = do
   rval <- evalExpr r
   return $ VBool (lval == rval)
 
-evalExpr (Subscript (Variable name) e) = do
+evalExpr ss@(Subscript (Variable name) e) = do
   index <- evalExpr e
   st <- getSymTable
   let var = Map.lookup name st
   return $ case (var, index) of
     (Just (VArray a), VInt i) -> findWithDefault a i VNull
     (Just (VHash h), VString s) -> Map.findWithDefault VNull s h
-    _ -> VTest
+    _ -> todo "Can't do a subscript unless on array/int or hash/string" (ss, var, index)
 
 evalExpr (Assignment (LVar name) e) = do
   result <- evalExpr e
@@ -109,10 +125,55 @@ evalExpr (Assignment (LVar name) e) = do
   return result
 
 evalExpr (FunctionInvocation name args) = do
+  _ <- liftIO $ print args
   fn <- evalExpr name
   evaledArgs <- mapM evalExpr args
   code <- liftIO $ runFunction fn evaledArgs
   return $ code
+
+
+evalExpr (Pipe goods) = do
+  --goods2 :: [(Value, [Value])] <- mapM evalArgs goods
+
+  result <- liftIO $ do
+    let commands = [
+                     ("ls", [])
+                   , ("cat", [])
+                   , ("wc", ["-l"])
+                   , ("cat", ["-"])]
+
+    let nextStdin = Proc.NoStream
+    (nextStdin, h1) <- kickProc "p1" "ls" [] nextStdin
+    (nextStdin, h2) <- kickProc "p2" "cat" [] nextStdin
+    (nextStdin, h3) <- kickProc "p3" "wc" ["-l"] nextStdin
+    (nextStdin, h4) <- kickProc "p4" "cat" ["-"] nextStdin
+
+    _ <- Proc.waitForProcess h1
+    _ <- Proc.waitForProcess h2
+    _ <- Proc.waitForProcess h3
+    _ <- Proc.waitForProcess h4
+
+    let (Proc.UseHandle final) = nextStdin
+    stdout <- Handle.hGetContents final
+
+    print $ "stdout: " ++ stdout
+
+    return stdout
+
+  return $ VString result
+  where
+    evalArgs (FunctionInvocation name args) = do
+      n <- eval2Str name
+      as <- mapM eval2Str args
+      return (n, as)
+    evalArgs e = todo "how do we invoke non-FunctionInvocations" e
+    kickProc name cmd args stdin = do
+      let p = (Proc.proc cmd args) {
+                 Proc.std_in = stdin
+               , Proc.std_out = Proc.CreatePipe
+               , Proc.close_fds = True }
+      (_, Just out, _, procH) <- Proc.createProcess_ name p
+      return (Proc.UseHandle out, procH)
 
 evalExpr (Integer i) = return $ VInt i
 evalExpr (Str i) = return $ VString i
@@ -122,7 +183,7 @@ evalExpr e = do
   liftIO $ debug "an unsupported expression was found"
   liftIO $ debug e
   _ <- error "ending early"
-  return VTest
+  return $ todo "an unsupported expression was found" e
 
 
 runFunction :: Value -> [Value] -> IO Value
@@ -130,6 +191,6 @@ runFunction fn args = do
   code <- case fn of
             VString str -> do
                   debug $ "Calling function: " ++ str ++ show args
-                  Cmd.rawSystem str (map show args)
+                  Proc.rawSystem str (map show args)
             _ -> return $ Exit.ExitFailure (-1)
   return $ VPacket code
