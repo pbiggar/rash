e0dce14d4cc2ad3529d1f6857bce89754e5da677
Paul Biggar <paul.biggar@gmail.com>
2016-09-21 16:23:52 -0400

    Catch the exitcode thrown by sys.exit

--- src/Rash/Runner.hs
+++ src/Rash/Runner.hs
@@ -1,9 +1,12 @@
 module Rash.Runner (runSource, runFile, evalAndPrint) where
 
 import qualified Data.Either as Either
+import qualified Data.Maybe as Maybe
 import qualified System.Exit as Exit
 import qualified Text.Groom as G
-import           Control.Monad (when)
+import           Control.Monad (when, liftM)
+import           Control.Exception (catch, fromException)
+
 
 import Rash.AST
 import Rash.Runtime
@@ -16,15 +19,20 @@ evalAndPrint :: String -> String -> IO ()
 evalAndPrint name source = do
   result <- case (Bash2AST.translate name source) of
     Left err -> return . show $ err
-    Right prog -> Interpreter.interpret prog [] >>= return .show
+    Right prog -> Interpreter.interpret prog [] >>= return . show
   putStrLn result
   return ()
 
 runProgram :: Program -> [String] -> IO Exit.ExitCode
 runProgram program args = do
   when (Options.flags_debug) $ putStrLn $ (G.groom program)
-  exit <- Interpreter.interpret program args
-  return $ convertToExitCode exit
+  catch
+    (liftM convertToExitCode (Interpreter.interpret program args))
+    -- sys.exit maybe throw an ExitCode in an exception
+    (\e -> return $
+             Maybe.fromMaybe (Exit.ExitFailure (-1)) (fromException e))
+
+
 
 runSource :: String -> String -> [String] -> IO Exit.ExitCode
 runSource name source args = do
@@ -32,9 +40,8 @@ runSource name source args = do
     (\err -> (do
                 print err
                 return $ Exit.ExitFailure (-1)))
-    (\prog -> runProgram prog args)
+    (\prog -> (runProgram prog args))
     (Bash2AST.translate name source)
-
 
 runFile :: FilePath -> IO Exit.ExitCode
 runFile file = do
